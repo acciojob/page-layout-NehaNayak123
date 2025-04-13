fd1b455afb2a719f56d61efb9a40e27f7dcb09ef
NehaNayak123 <111532433+NehaNayak123@users.noreply.github.com>
2025-04-14 02:47:36 +0530

    Update PageLayout.jsx

--- src/components/PageLayout.jsx
+++ src/components/PageLayout.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+// import React from 'react';
 
-const PageLayout = ({ header, children, footer }) => {
+// const PageLayout = ({ header, children, footer }) => {
+//   return (
+//     <div className="page-layout">
+//       {header}
+//       <main>{children}</main>
+//       {footer}
+//     </div>
+//   );
+// };
+
+// export default PageLayout;
+
+// PageLayout.js
+import React from "react";
+
+const PageLayout = ({ header, footer, children }) => {
   return (
-    <div className="page-layout">
-      {header}
+    <div>
+      <header>{header}</header>
+      <br />
       <main>{children}</main>
-      {footer}
+      <br />
+      <footer>{footer}</footer>
     </div>
   );
 };
